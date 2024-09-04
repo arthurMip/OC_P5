278e99a88cd858d797bd707a7d904934559828b3
Arthur <arthur.tripard@wearemip.com>
2024-09-04 18:59:06 +0200

    configure entities relations

--- ExpressVoitures/Data/ApplicationDbContext.cs
+++ ExpressVoitures/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ExpressVoitures.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExpressVoitures.Data
@@ -8,6 +9,43 @@ namespace ExpressVoitures.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<BuyingInfo> BuyingInfos { get; set; }
+        public DbSet<FixingInfo> FixingInfos { get; set; }
+        public DbSet<SellingInfo> SellingInfos { get; set; }
+        public DbSet<ManufacturingInfo> ManufacturingInfos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Car>()
+                .HasMany(c => c.Images)
+                .WithOne()
+                .HasForeignKey(i => i.CarId);
+
+            builder.Entity<Car>()
+                .HasOne(c => c.BuyingInfo)
+                .WithOne()
+                .HasForeignKey<BuyingInfo>(b => b.CarId);
+
+            builder.Entity<Car>()
+                .HasOne(c => c.FixingInfo)
+                .WithOne()
+                .HasForeignKey<FixingInfo>(f => f.CarId);
+
+            builder.Entity<Car>()
+                .HasOne(c => c.SellingInfo)
+                .WithOne()
+                .HasForeignKey<SellingInfo>(s => s.CarId);
+
+            builder.Entity<Car>()
+                .HasOne(c => c.ManufacturingInfo)
+                .WithOne()
+                .HasForeignKey<ManufacturingInfo>(m => m.CarId);
         }
     }
 }
