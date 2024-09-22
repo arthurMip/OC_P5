9c8eeec8e80dbf577108926c65450ee80cd52163
Arthur <arthur.tripard@wearemip.com>
2024-09-22 16:17:46 +0200

    added admin role and account

---
+++ ExpressVoitures/Helpers/RoleInitializer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ExpressVoitures.Helpers;
+
+public static class RoleInitializer
+{
+    public static async Task CreateRolesAndAdminUser(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+    {
+        // Define the roles to be created
+        string[] roleNames = ["Admin", "User"];
+
+        foreach (var roleName in roleNames)
+        {
+            // Check if the role exists, if not create it
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+        }
+
+        // Create an Admin user if it doesn't exist
+        const string adminEmail = "admin@voiture-express.fr";
+        IdentityUser? adminUser = await userManager.FindByEmailAsync(adminEmail);
+        if (adminUser is null)
+        {
+            var admin = new IdentityUser
+            {
+                UserName = adminEmail,
+                Email = adminEmail
+            };
+            var createAdmin = await userManager.CreateAsync(admin, "AdminPassword123!");
+
+            if (createAdmin.Succeeded)
+            {
+                // Assign the Admin role to the user
+                await userManager.AddToRoleAsync(admin, "Admin");
+            }
+        }
+    }
+}
--- ExpressVoitures/Program.cs
+++ ExpressVoitures/Program.cs
@@ -1,5 +1,6 @@
 using ExpressVoitures.Data;
 using ExpressVoitures.Data.Repositories;
+using ExpressVoitures.Helpers;
 using ExpressVoitures.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -13,14 +14,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services
-    .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.LoginPath = "/Account/Login";
-});
+builder.Services.ConfigureApplicationCookie(options => options.LoginPath = "/Account/Login");
 
+
+builder.Services.AddRazorPages();
 
 builder.Services.AddControllersWithViews();
 
@@ -41,16 +41,37 @@ else
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Account}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+
+
+try
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+        await RoleInitializer.CreateRolesAndAdminUser(roleManager, userManager);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred while seeding roles");
+    Console.WriteLine(ex.Message);
+}
+
 
 app.Run();
